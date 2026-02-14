526ee6a421cf6ade81b45f08c9fe2c3afeb4d5a4
Anvar Artemov <artemovanvar2@gmail.com>
2026-02-14 14:46:01 +0100

    build(docker): clean up files

--- core_service/app/database.py
+++ core_service/app/database.py
@@ -3,7 +3,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from app.config import settings  # Импортируем настройки
 
 client = AsyncIOMotorClient(settings.MONGO_URL)
-db = client.library_databasee
+db = client.library_database
 
 # Определяем коллекции
 books_collection = db.get_collection("books")
